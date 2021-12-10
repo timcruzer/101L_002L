a900bb132a23de2750d9cd19b45062fa548295ca
timcruzer <tcc3t7@umsystem.edu>
2021-12-09 20:31:43 -0600

    LAST LAB ASSIGNMENT

---
+++ Grades.py
@@ -0,0 +1,23 @@
+import math
+
+def total(values):
+    total = 0
+    for num in values:
+        total+=num
+    return float(total)
+
+def average(values):
+    if len(values) == 0:
+        return math.nan
+    total = 0
+    for num in values:
+        total+=num
+    return float(total / len(values))
+
+def median(values):
+    values.sort()
+    center = len(values) / 2
+    if center // 1 == center: #even number of elements in list
+        return (values[int(center - 0.5)] + values[int(center + 0.5)]) / 2
+    else: #odd number of elements in list
+        return values[int(center)]
---
+++ Lab Assignment Week 15.py
@@ -0,0 +1,35 @@
+import unittest
+import Grades
+import math
+
+class Grade_Test(unittest.TestCase):
+
+    def test_total_returns_total_of_list(self):
+        result = Grades.total([1, 10, 22])
+        self.assertEqual(result, 33, "The total function should return 33")
+    
+    def test_total_returns_0(self):
+        result = Grades.total([])
+        self.assertEqual(result, 0, "The total function should return 0")
+
+    def test_average_one(self):
+        result = Grades.average([2,5,9])
+        self.assertAlmostEqual(result, 5.33333, 5, "The average function should return 5.33333")
+
+    def test_average_two(self):
+        result = Grades.average([2, 15, 22, 9])
+        self.assertAlmostEqual(result, 12.0000, 4, "The average function should return 12.0000")
+
+    def test_average_returns_nan(self):
+        result = Grades.average([])
+        self.assertIs(result, math.nan, "The average function should return math.nan")
+    
+    def test_median_one(self):
+        pass
+
+    def test_median_two(self):
+        pass
+
+    
+
+unittest.main()
